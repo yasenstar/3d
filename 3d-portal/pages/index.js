7e920f8cc8510e638929758614771764c0bf49c4
Yasen <xiaoqizhao@outlook.com>
2023-01-02 14:55:19 -0500

    add imageMap with multiple page structure

--- 3d-portal/pages/index.js
+++ 3d-portal/pages/index.js
@@ -15,48 +15,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+        <h1>Portal of 3D Modeling</h1>
+
+        <div className={styles.grid}>
+          <a
+            href="./bizarch"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Biz Arch <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Find more aspects on Business Architecture.
+            </p>
+          </a>
         </div>
 
         <div className={styles.grid}>
