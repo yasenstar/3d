82dcc131d32decdf3e85b9361f9208beea6c33ee
Yasen <xiaoqizhao@outlook.com>
2023-01-02 15:04:30 -0500

    add build

--- 3d-portal/pages/bizarch/index.js
+++ 3d-portal/pages/bizarch/index.js
@@ -5,7 +5,7 @@ const BizArch = () => {
     return <div className={styles.main}>
         <h1>Business Architecture</h1>
         <div>
-            <Image src='/bizbok.jpeg' height="550" width="800" useMap="#myMap"></Image>
+            <Image src='/bizbok.jpeg' alt="BizArch Framework" height="550" width="800" useMap="#myMap"></Image>
         </div>
         <map name="myMap">
             <area shape="rect" coords="10,10,50,50" href="https://www.amazon.ca"></area>
